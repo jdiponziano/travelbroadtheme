515d4104bfd083d65d07ff26a757d6a24a612419
Jessica Di Ponziano <jdiponziano@gmail.com>
2016-03-31 16:55:54 -0400

    Add front page html structure

---
+++ front-page.php
@@ -0,0 +1,85 @@
+<article>
+  <section class="home-hero">
+  <img src="#" alt="Kae Lani" />
+  <h1>Hi I am Kae Lani</h1>
+  <p>
+    Paragraph text about Kae Lani
+  </p>
+  <a href="/about.html">About Me</a>
+  </section>
+  <section class="home-events">
+    <h2>Upcoming Events</h2>
+    <div class="event-listing">
+      <ul>
+        <li>
+          <div class="event-month">Dec</div>
+          <div class="event-day">12</div>
+          <div class="event-name">Cool event</div>
+          <div class="location">NY</div>
+          <div class="time">10:30am</div>
+          <div class="event-btn"><a href="#">Event Details</a></div>
+        </li>
+        <li>
+          <div class="event-month">Dec</div>
+          <div class="event-day">12</div>
+          <div class="event-name">Cool event</div>
+          <div class="location">NY</div>
+          <div class="time">10:30am</div>
+          <div class="event-btn"><a href="#">Event Details</a></div>
+        </li>
+      </ul>
+    </div>
+    <div class="event-video">
+
+    </div>
+    <a href="#">View All Events</a>
+  </section>
+  <section class="home-blog">
+    <h2>Latest on the Blog</h2>
+    <div class="cards">
+      <div class="card">
+        <div class="card-image">
+          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains.png" alt="">
+        </div>
+        <div class="card-header">
+          First Card
+        </div>
+        <div class="card-copy">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-image">
+          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains-4.png" alt="">
+        </div>
+        <div class="card-header">
+          Another Card
+        </div>
+        <div class="card-copy">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, officiis sunt neque facilis culpa molestiae necessitatibus delectus veniam provident.</p>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-image">
+          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/mountains-3.png" alt="">
+        </div>
+        <div class="card-header">
+          The Last Card
+        </div>
+        <div class="card-copy">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+    <a href="#">Visit Blog</a>
+  </section>
+  <section class="home-photos">
+    <h2>Photography</h2>
+    <p>
+      Check out the latest photography
+    </p>
+    <a href="#">View Gallery</a>
+  </section>
+</article>
