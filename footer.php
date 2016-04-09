fee962d9d75ff1bbdf75bf5b4e4a49caf2748ec7
Jessica Di Ponziano <jdiponziano@gmail.com>
2016-04-09 18:06:50 -0400

    Footer structure and styles generated

--- footer.php
+++ footer.php
@@ -12,25 +12,40 @@
 ?>
 
 	</div><!-- #content -->
-
+	<section class="footer-widgets">
+		<div class="wrap">
+			<div class="footer-widget-left">
+				<h4>Widget Title</h4>
+				<p>
+					Love to play with owner's hair tie. Lick the other cats lick sellotape. Pee in the shoe. Favor packaging over toy lick yarn hanging out of own butt sniff other cat's butt and hang jaw half open thereafter. Hide head under blanket so no one can see sleep on dog bed, force dog to sleep on floor. Eat from dog's food why must they do that, shake treat bag, and unwrap toilet paper damn that dog poop in litter box, scratch the walls scratch the furniture.
+				</p>
+			</div>
+			<div class="footer-widget-center">
+				<h4>Widget Title</h4>
+				<p>
+					Love to play with owner's hair tie. Lick the other cats lick sellotape. Pee in the shoe. Favor packaging over toy lick yarn hanging out of own butt sniff other cat's butt and hang jaw half open thereafter. Hide head under blanket so no one can see sleep on dog bed, force dog to sleep on floor. Eat from dog's food why must they do that, shake treat bag, and unwrap toilet paper damn that dog poop in litter box, scratch the walls scratch the furniture.
+				</p>
+			</div>
+			<div class="footer-widget-right">
+				<h4>Widget Title</h4>
+				<p>
+					Love to play with owner's hair tie. Lick the other cats lick sellotape. Pee in the shoe. Favor packaging over toy lick yarn hanging out of own butt sniff other cat's butt and hang jaw half open thereafter. Hide head under blanket so no one can see sleep on dog bed, force dog to sleep on floor. Eat from dog's food why must they do that, shake treat bag, and unwrap toilet paper damn that dog poop in litter box, scratch the walls scratch the furniture.
+				</p>
+			</div>
+		</div>
+	</section>
 	<footer class="site-footer">
 		<div class="wrap">
-			<div class="footer-widgets">
-				<div class="footer-widget-left">
-
-				</div>
-				<div class="footer-widget-center">
-
-				</div>
-				<div class="footer-widget-right">
-
-				</div>
-
-			</div>
 			<div class="site-info">
 				<?php atravelbroad_do_copyright_text(); ?>
 			</div>
-
+			<div class="footer-menu">
+				<ul class="alignright">
+					<li>Advertise</li>
+					<li>Guest Post</li>
+					<li>Contact</li>
+				</ul>
+			</div>
 		</div><!-- .wrap -->
 	</footer><!-- .site-footer -->
 </div><!-- #page -->
