fbc6d4de6f90cf4675f84ab642a44bc8540d9fe7
Jessica DiPonziano <jdiponziano@gmail.com>
2016-10-31 11:35:58 -0400

    Add blog thumb image size

--- functions.php
+++ functions.php
@@ -161,4 +161,9 @@ require get_template_directory() . '/inc/scripts.php';
 /**
  * Crop Images Sizes.
  */
- add_image_size('galleryImageCropped', 300, 300, true);
+
+add_action( 'after_setup_theme', 'atb_add_image_sizes' );
+function atb_add_image_sizes() {
+    add_image_size('galleryImageCropped', 300, 300, true);
+    add_image_size( 'blog-thumb', 9999, 300, true ); // (cropped)
+}
